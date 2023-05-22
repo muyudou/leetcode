403e4f7657d32234253aac80743ef56fc3784c08
muyudou <muyudou@gmail.com>
2023-05-22 20:38:10 +0800

    add 235/702

---
+++ binary-tree/701.二叉搜索树中的插入操作.js
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.cn/problems/insert-into-a-binary-search-tree/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    if (!root) {
+        const newNode = new TreeNode(val);
+        return newNode;
+    }
+    if (root.val < val) {
+        root.right = insertIntoBST(root.right, val);
+    } else {
+        root.left = insertIntoBST(root.left, val);
+    }
+    return root;
+};
