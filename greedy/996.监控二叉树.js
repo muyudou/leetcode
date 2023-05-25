77e85ed783b8b878b92fe78b69d1076ca0c72294
muyudou <muyudou@gmail.com>
2023-05-25 11:15:01 +0800

    update 996

--- greedy/996.监控二叉树.js
+++ greedy/996.监控二叉树.js
@@ -10,6 +10,12 @@
 /**
  * @param {TreeNode} root
  * @return {number}
+ * 0: 无覆盖 1: 有摄像头 2: 有覆盖
+ * 1. 叶子节点返回有覆盖2，因为父节点要安装摄像头
+ * 2. 如果左右2个子节点都有覆盖，则父节点为无覆盖为0
+ * 3. 如果左孩子或者右孩子，有一个未覆盖，则父节点需要安装摄像头，返回1，result++
+ * 4. 如果左孩子或者右孩子有一个有摄像头，则父节点有覆盖，返回2
+ * 5. 最后根节点，如果返回了0，则需要添加一个摄像头，result++
  */
 var minCameraCover = function(root) {
     let result = 0;
@@ -21,6 +27,7 @@ var minCameraCover = function(root) {
         }
         const left = traverse(root.left);
         const right = traverse(root.right);
+        // 左右有覆盖，父节点为无覆盖
         if (left === 2 && right === 2) {
             return 0;
         }
